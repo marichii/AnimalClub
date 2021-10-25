4ac38cc50ea5ef7273a09dbe263bac92eddca132
BrAdLy HaRpEr <92391430+TheTamTM@users.noreply.github.com>
2021-10-25 17:27:12 +0000

    modified mob.java and added zombie.java, beginning work on mobs and hopefully combat

--- Java/mobs/mob.java
+++ Java/mobs/mob.java
@@ -3,7 +3,6 @@ public abstract class mob {
   int HP = 50;
   int ATK = 15;
   int DEF = 10; 
-  int SPD = 10; 
+  int SPD = 10;
   int CRIT = 1; //TBA 
- 
 }
---
+++ Java/mobs/zombie.java
@@ -0,0 +1,3 @@
+public class zombie extends mob {
+  
+}
