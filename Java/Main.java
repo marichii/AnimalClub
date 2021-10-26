3c356c86a2a4dd0abaa6d1c2f5c835069fa87d39
BrAdLy HaRpEr <92391430+TheTamTM@users.noreply.github.com>
2021-10-26 17:32:23 +0000

    i added character select (without characters)

--- Java/Main.java
+++ Java/Main.java
@@ -1,15 +1,21 @@
-import java.util.*;
+import java.util.*; //Add CPU or 2P mode
 
 public class Main {
   
   public static void main(String[] args) {
-  Scanner userChoice = new Scanner(System.in);
+  Scanner modeChoice = new Scanner(System.in);
+  Scanner characterChoice = new Scanner (System.in);
   String gameMode;
+  String Character;
 
   System.out.println("Welcome to AnimalClub!");
   System.out.println("Would you like to play? (SAY PLAY TO START)");
-  gameMode = userChoice.next();
+  gameMode = modeChoice.next();
 
-  if (gameMode = "PLAY")
+  if (gameMode.equalsIgnoreCase("play"))
+  {
+    System.out.println("Choose your character!");
+    Character = characterChoice.next();
+  }
   }
 }
--- Java/mobs/zombie.java
+++ Java/mobs/zombie.java
@@ -1,3 +1,3 @@
 public class zombie extends mob {
-  
+  //zombies burn in daylight, cant swim, but they dont get exhausted
 }
