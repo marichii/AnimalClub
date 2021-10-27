7e52b18abd3397962b52c644113438bcf47beb5f
BrAdLy HaRpEr <92391430+TheTamTM@users.noreply.github.com>
2021-10-27 17:30:51 +0000

    added slime

--- Java/mobs/slime.java
+++ Java/mobs/slime.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class slime implements mob {
   public void creatureHP(){
     HP = 60;
@@ -13,23 +12,6 @@ public class slime implements mob {
     SPD = 7;
   }
   public void creatureEnvironment(){
-=======
-public class slime extends mob {
-//sticky, steals your item, high hp, low atk
-  public void creatureHP(){
-    HP = 35;
-  }
-  public void creatureATK(){
-    ATK = 10;
-  }
-  public void creatureDEF(){
-    DEF = 8;
-  }
-  public void creatureSPD(){
-    SPD = 9;
-  }
-  public void creatureEnvironment(){
-    
->>>>>>> origin/assignment
+
   }
 }
