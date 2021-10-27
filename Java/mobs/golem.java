386cedaf8f776ee8f225c3628264bde2c4f780a2
Mariam Al <92391405+marichii@users.noreply.github.com>
2021-10-27 17:02:34 +0000

    setting up mobs

--- Java/mobs/golem.java
+++ Java/mobs/golem.java
@@ -1,2 +1,4 @@
 public class golem extends mob {
+ 
+}
 //high def + high atk, low spd
--- Java/mobs/mob.java
+++ Java/mobs/mob.java
@@ -1,9 +1,7 @@
-public abstract class mob {
-
-  String
-  int HP = 50;
-  int ATK = 15;
-  int DEF = 10; 
-  int SPD = 10;
-  int CRIT = 1; //TBA 
+interface mob {
+  public void creatureHP();
+  public void creatureATK();
+  public void creatureDEF();
+  public void creatureSPD();
+  public void creatureEnvironment();
 }
